1ee8b696b75192bb13a3e0c7e394d5b006a30399
adashuai5 <ad873606339@gmail.com>
2020-03-29 14:49:31 +0800

    vue-cli 升级

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import App from './App'
-import router from './router'
-import store from './store'
-import Util from '@/helpers/util.js'
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import App from "./App";
+import router from "./router";
+import store from "./store";
+import Util from "@/helpers/util.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(ElementUI);
-Vue.use(Util)
+Vue.use(Util);
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/Vue-Blog/'
-    : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/blog-client/" : "/"
+};
